77b57a26a178fac74e0bd0781b5ad8a2f1309584
Matias-Ignacio <matias.riveiro@est.fi.uncoma.edu.ar>
2023-08-30 18:57:40 -0300

    menu principal

--- Estructuras/header.php
+++ Estructuras/header.php
@@ -1,12 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Grupo 5</title>
-    
+    <title><?php $Titulo ?></title>
+    <!--bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <!-- Enlaza FontAwesome CSS (Asegúrate de usar la URL correcta) -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">  
 </head>
 <body>
-    
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                TP1
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="./tp1/ejercicio1/vista/index.php">Ejercicio 1</a></li>
+                <li><a class="dropdown-item" href="./tp1/ejercicio2/vista/index.php">Ejercicio 2</a></li>
+                <li><a class="dropdown-item" href="./tp1/ejercicio3/vista/index.php">Ejercicio 3</a></li>
+                <li><a class="dropdown-item" href="./tp1/ejercicio4/vista/index.php">Ejercicio 4</a></li>
+                <li><a class="dropdown-item" href="./tp1/ejercicio5/vista/index.php">Ejercicio 5</a></li>
+                <li><a class="dropdown-item" href="./tp1/ejercicio6/vista/index.php">Ejercicio 6</a></li>
+                <li><a class="dropdown-item" href="./tp1/ejercicio7/vista/index.php">Ejercicio 7</a></li>
+                <li><a class="dropdown-item" href="./tp1/ejercicio8/vista/index.php">Ejercicio 8</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                TP2
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="./tp2/ejercicio2/tp1ejercicio1/vista/index.php">TP1 Ej 1</a></li>
+                <li><a class="dropdown-item" href="./tp2/ejercicio2/tp1ejercicio2/vista/index.php">TP1 Ej 2</a></li>
+                <li><a class="dropdown-item" href="./tp2/ejercicio2/tp1ejercicio3/vista/index.php">TP1 Ej 3</a></li>
+                <li><a class="dropdown-item" href="./tp2/ejercicio2/tp1ejercicio4/vista/index.php">TP1 Ej 4</a></li>
+                <li><a class="dropdown-item" href="./tp2/ejercicio2/tp1ejercicio5/vista/index.php">TP1 Ej 5</a></li>
+                <li><a class="dropdown-item" href="./tp2/ejercicio2/tp1ejercicio6/vista/index.php">TP1 Ej 6</a></li>
+                <li><a class="dropdown-item" href="./tp2/ejercicio2/tp1ejercicio7/vista/index.php">TP1 Ej 7</a></li>
+                <li><a class="dropdown-item" href="./tp2/ejercicio2/tp1ejercicio8/vista/index.php">TP1 Ej 8</a></li>                
+                <li><a class="dropdown-item" href="./tp2/ejercicio3ABC/vista/index.php">Ejercicio 3</a></li>
+                <li><a class="dropdown-item" href="./tp2/ejercicio4/vista/index.php">Ejercicio 4</a></li>
+            </ul> 
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                TP3
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="./tp3/ejercicio1/vista/index.php">Ejercicio 1</a></li>
+                <li><a class="dropdown-item" href="./tp3/ejercicio2/vista/index.php">Ejercicio 2</a></li>
+                <li><a class="dropdown-item" href="./tp3/ejercicio3/vista/index.php">Ejercicio 3</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 </body>
 </html>
--- tp1/ejercicio1/vista/index.php
+++ tp1/ejercicio1/vista/index.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+    $Titulo = "GRUPO 5 TP1 Ej1";
+    include_once("../../../Estructuras/header.php");
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    
 </head>
 <body>
 <script type="text/javascript">
